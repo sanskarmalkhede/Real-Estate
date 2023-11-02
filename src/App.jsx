50e2577508413c65dd94a8d770587a5559161249
Sanskar Malkhede <malkhedesanskar@gmail.com>
2023-11-03 01:06:14 +0530

    created pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from "react";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
 
 export default function App() {
   return (
-    <div className='text-sky-400'>App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="signin" element={<SignIn />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="about" element={<About />} />
+        <Route path="profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
