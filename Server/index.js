abd65aff03f38ceee482dc41d2afc36253ff8039
Sanskar Malkhede <malkhedesanskar@gmail.com>
2023-11-03 03:35:42 +0530

    mongoose dependencies, connect to MongoDB

--- Server/index.js
+++ Server/index.js
@@ -1,5 +1,12 @@
 import express from 'express';
+import mongoose, { connect } from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Connected to MongoDB!');
+    }
+);
 
 const app = express();
 
